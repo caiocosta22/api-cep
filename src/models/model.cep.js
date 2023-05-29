8f1a8f5611fccb98e8781936ab7c2f739b405b03
caiocosta22 <caiocosta22@gmail.com>
2023-05-28 23:43:33 -0300

    Insercao, Consulta, e Consulta/Insercao feita

--- src/models/model.cep.js
+++ src/models/model.cep.js
@@ -1,7 +1,44 @@
+import { query } from "express";
 import sqlConfig from "../config/dbconfig.js";
 import sql from "mssql"
 function Inserir(dadosCep, callback){
-    sql.connect(function(err, conn){
+    console.log("🚀 ~ file: model.cep.js:4 ~ Inserir ~ dadosCep:", dadosCep)
+    sql.connect(sqlConfig).then(() => {
+            console.log('Conectado ao banco de dados!');
+            // Executando o INSERT
+            const request = new sql.Request();
+            let ssql = "INSERT INTO dbo.CONSULTA_CEP(cep, logradouro, complemento, bairro, localidade, uf, ibge, gia, ddd, siafi) ";
+            // ssql +="values(?,?,?,?,?,?,?,?,?,?) ";
+            const { cep, logradouro, complemento, bairro, localidade, uf, ibge, gia, ddd, siafi } = dadosCep
+            ssql +=`VALUES(
+                    '${cep}', 
+                    '${logradouro}', 
+                    '${complemento}', 
+                    '${bairro}', 
+                    '${localidade}', 
+                    '${uf}', 
+                    '${ibge}',
+                    '${gia}',
+                    '${ddd}',
+                    '${siafi}'
+                )`;
+        request.query(ssql)
+        .then((result) => {
+            console.log('Registro inserido com sucesso!');
+            return result
+        }).catch((err) => {
+            console.error('Erro ao inserir registro:', err);
+            return err
+        }).finally(() => {
+            // Encerrando a conexão com o banco de dados
+            sql.close();
+            return;
+        });
+    }).catch((err) => {
+            console.error('Erro ao conectar ao banco de dados:', err);
+            return err;
+    })};
+    /*sql.connect(function(err, conn){
         conn.beginTransaction(function(err){
                 
             let ssql = "insert into CONSULTA_CEP(cep, logradouro, complemento, bairro, localidade, uf, ibge, gia, ddd, siafi) ";
@@ -22,6 +59,6 @@ function Inserir(dadosCep, callback){
             conn.release();
         });
     });
-};
+};*/
 
-export default {Inserir}
+export default { Inserir }
